9fd52d4e70f16a7e0ca8bc0964d135d64d0e3b59
Ym00d <mattvii.bichck@gmail.com>
2025-10-18 11:50:02 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Hello World")
-print("Hello I'm Github")
+#print("Hello World")
+#print("Hello I'm Github")
+
+#ООП Класи Артибути та методи класів
+
+class Student:
+    print("Hello!")
+    def __init__(self, height=160):
+        self.height=160
+        self.age=17
+
+nick=Student()
+kate=Student(height=155)
+print(nick.height)
+print(nick.age)
